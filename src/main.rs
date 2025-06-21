1dd6cce1b544b9c3bf00b0fed6253b487cf69ede
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-21 11:30:40 +0300

    feat: genesis block generator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use chrono::{DateTime, Local, NaiveDate, NaiveDateTime, Utc};
-use serde::{Deserialize, Serialize};
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::fs;
+use chrono::{DateTime, Utc};
+// use serde::{Deserialize, Serialize};
+use sha2::{Sha256, Digest};
 
 #[derive(Debug)]
 struct Block {
@@ -14,18 +12,18 @@ struct Block {
     timestamp: DateTime<Utc>
 }
 
-#[derive(Serialize, Deserialize)]
-struct MiningState {
-    block_index: u64,
-    last_nonce: u64,
-}
+// #[derive(Serialize, Deserialize)]
+// struct MiningState {
+//     block_index: u64,
+//     last_nonce: u64,
+// }
 
 fn main() {
-    // let mut blockchain: Vec<Block> = Vec::new();
+    let mut blockchain: Vec<Block> = Vec::new();
 
     let genesis_block: Block = create_genesis_block();
-
     println!("Genesis Block: {:#?}", genesis_block);
+    blockchain.push(genesis_block);
 }
 
 fn create_genesis_block() -> Block {
@@ -33,36 +31,39 @@ fn create_genesis_block() -> Block {
         index: 0,
         data: String::from("Genesis Block"),
         hash: String::new(),
-        previous: "0".repeat(16),
+        previous: "0".repeat(64),
         nonce: 0,
         timestamp: Utc::now()
     };
 
-    genesis_block.nonce = find_nonce(&genesis_block, 0);
+    let nonce_hash = find_nonce(&genesis_block);
+    genesis_block.nonce = nonce_hash.0;
+    genesis_block.hash = nonce_hash.1;
 
     genesis_block
 }
 
 fn hash(input: &str) -> String {
-    let mut hasher = DefaultHasher::new();
-    input.hash(&mut hasher);
-    format!("{:x}", hasher.finish())
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    format!("{:x}", result)
 }
 
-fn find_nonce(block: &Block, block_index: u64) -> u64 {
+fn find_nonce(block: &Block) -> (u64, String) {
     let mut nonce = 0;
     let data = format!("{}{}", nonce, block.data);
     let mut hash_string = hash(&data);
     
-    while !hash_string.starts_with("0") {
+    while !hash_string.starts_with("000") {
         nonce += 1;
         let data = format!("{}{}", nonce, block.data);
         hash_string = hash(&data);
         
-        if nonce % 100_000 == 0 {
-            println!("Trying nonce: {}", nonce);
+        if nonce % 1_000_000 == 0 {
+            println!("Nonce: {} / Hash: {}", nonce, hash_string);
         }
     }
 
-    nonce
+    (nonce, hash_string)
 }
